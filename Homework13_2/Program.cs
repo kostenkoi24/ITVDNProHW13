fe8d5e65ed6a5b5cc71663b57e2e05116ae13db9
kostenkoi24 <kostenko.i@gmail.com>
2023-02-23 22:22:15 +0200

    Homework13

--- Homework13_2/Program.cs
+++ Homework13_2/Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace Homework13_2
 {
     class Program
     {
+
+        static void Method1()
+        {
+            Console.WriteLine("Method1: запущений.");
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write("1");
+            }
+            Console.WriteLine("Method1: завершено.");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("Method2: запущений.");
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write("2");
+            }
+            Console.WriteLine("Method2: завершено.");
+        }
+
+
+        static void PMethod()
+        {
+            ParallelOptions options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 2; 
+            Parallel.Invoke(options, Method1, Method2);
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Основний потік запущений.");
+
+            Console.WriteLine("Кількість логічних ядер CPU:" + Environment.ProcessorCount);
+
+            Console.ReadKey();
+
+            
+            Action action = new Action(PMethod);
+
+            Task task = new Task(action);
+            task.Start();
+
+            Console.WriteLine("Main(): запущений.");
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write("Main()");
+            }
+            Console.WriteLine("Main(): завершено.");
+
+
+            Console.WriteLine("Основний потік завершено.");
+
+            // Delay
+            Console.ReadKey();
         }
     }
 }
---
+++ Homework13_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Homework13_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = 1000000;
+
+            int[] array = new int[arraySize];
+
+            Random random = new Random();
+
+            // Ініціалізація масиву даних є позитивними значеннями.
+            Parallel.For(0, arraySize, (i) => array[i] = random.Next(1, 10));
+
+            
+
+            // Запит PLINQ для пошуку непарні числа.
+            ParallelQuery<int> negatives = from element in array.AsParallel()
+                                           where element % 2 == 0
+                                           select element;
+
+            foreach (int element in negatives)
+                Console.Write(element + " ");
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
